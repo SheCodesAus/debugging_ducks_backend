fc97d1f7b7d1d5b02bc56650362a08a870d06667
Afira <afiratahmali@gmail.com>
2025-01-17 23:28:02 +0800

    added user serializer, CustomUserList & CustomUserDetail views, urlpatterns

--- shoppinglist/shoppinglist/urls.py
+++ shoppinglist/shoppinglist/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
