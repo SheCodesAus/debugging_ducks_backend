56886f6faf0c4d45625f1461efda859432ba2bb1
lisa.todesco <todesco@gmail.com>
2025-01-18 16:21:45 +0800

    ListCategory and ListIndivdual models created

--- shoppinglist/lists/models.py
+++ shoppinglist/lists/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class ListCategory(models.Model):
+    category_budget = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    category_name = models.CharField(max_length=255, blank=True)
+    archived_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='category_owner_modifier' 
+    )
+    archived_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='category_owner_archiver' 
+    )
+    created_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='category_owner_creator' 
+    )
+
+class ListIndividual(models.Model):
+    individual_budget = models.IntegerField(blank=True, null=True)
+    archived_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    notes = models.CharField(max_length=255, blank=True)
+    image = models.URLField(blank=True, null=True)
+    category_id = models.ForeignKey(
+       'ListCategory',
+       on_delete=models.CASCADE,
+       related_name='list_category' 
+    )
+    list_owner = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='list_owner' 
+    )
+    modified_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='list_owner_modifier' 
+    )
+    archived_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='list_owner_archiver' 
+    )
+    created_by = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='list_owner_creator' 
+    )
+    
