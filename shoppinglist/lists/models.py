25f648770c5c2f0fe3f77a0e79c33ada0ff12403
Afira <afiratahmali@gmail.com>
2025-01-20 23:11:19 +0800

    re-ordered model fields & modified model field settings and related names

--- shoppinglist/lists/models.py
+++ shoppinglist/lists/models.py
@@ -1,76 +1,67 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class ListCategory(models.Model):
+    category_name = models.CharField(max_length=255)
     category_budget = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    category_name = models.CharField(max_length=255, blank=True)
-    archived_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category_owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="owned_categories",
+    )
+    modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     modified_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='category_owner_modifier',
-       null=True,
-       blank=True  
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="categories_modified",
+        null=True,
+        blank=True,
     )
+    archived_at = models.DateTimeField(blank=True, null=True)
     archived_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='category_owner_archiver',
-       null=True,
-       blank=True  
-    )
-    created_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='category_owner_creator',
-       null=True,
-       blank=True  
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="categories_archived",
+        null=True,
+        blank=True,
     )
 
+
 class ListIndividual(models.Model):
-    list_name = models.CharField(max_length=255, null=True)
-    individual_budget = models.IntegerField(blank=True, null=True)
-    archived_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    list_name = models.CharField(max_length=255)
     notes = models.CharField(max_length=255, blank=True)
     image = models.URLField(blank=True, null=True)
+    individual_budget = models.IntegerField(blank=True, null=True)
     category_id = models.ForeignKey(
-       'ListCategory',
-       on_delete=models.CASCADE,
-       related_name='list_category',
-       null=True,
-       blank=True 
+        "ListCategory",
+        on_delete=models.CASCADE,
+        related_name="lists",
+        null=True,
+        blank=True,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
     list_owner = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='list_owner_id',
-       null=True,
-       blank=True  
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="owned_lists",
+        null=True,
+        blank=True,
     )
+    modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     modified_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='list_owner_modifier',
-       null=True,
-       blank=True  
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="lists_modified",
+        null=True,
+        blank=True,
     )
+    archived_at = models.DateTimeField(blank=True, null=True)
     archived_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='list_owner_archiver',
-       null=True,
-       blank=True  
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="lists_archived",
+        null=True,
+        blank=True,
     )
-    created_by = models.ForeignKey(
-       get_user_model(),
-       on_delete=models.CASCADE,
-       related_name='list_owner_creator',
-       null=True,
-       blank=True  
-    )
-    
